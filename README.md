d54e19cc8c0bc17b4a721572dec4a43bd4548bfa
Vidhu Nath <vid.nath@berkeley.edu>
2021-03-07 19:24:36 -0500

    Initial commit for data, templates, and pipeline

---
+++ README.md
@@ -0,0 +1,5 @@
+Board Game Guru
+=====
+
+This is the final project for the [UC Berkeley's Capstone class](https://ischoolonline.berkeley.edu/data-science/curriculum/capstone/), taught by Puya Vahabi and Alberto Todeschini in the Data Science Masters (MIDS) program.
+
---
+++ app.py
@@ -0,0 +1,73 @@
+#!usr/bin/env python 
+
+## Flask App
+# This is where Flask handles the data from the recommendation engine and passes it around the site to the correct location
+
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import recommendation
+
+import json
+import os
+import sys
+import turicreate as tc
+
+app = Flask(__name__)
+CORS(app)
+
+# Page redirects.
+@app.route("/")
+def root():
+    return render_template("index.html")
+
+@app.route("/product")
+def summary():
+    return render_template("product.html")
+
+
+@app.route("/process")
+def segment():
+    return render_template("process.html")
+
+
+@app.route("/people")
+def score():
+    return render_template("people.html")   
+
+
+# Import json of user's answers and run model prediction.
+@app.route("/recommend", methods=['GET'])
+def recommend():
+    json_file_path = "data/questionnaire_result.json"
+
+    with open(json_file_path, 'r') as j:
+         new_obs_data = json.loads(j.read())
+
+
+    new_obs_data_new = [int(i) for i in list(new_obs_data["games"].keys())]
+    filter_condt     = tc.SFrame({"age_min": [new_obs_data["age"]["min"]],
+                                  "age_max": [new_obs_data["age"]["max"]],
+                                  "num_players_min": [new_obs_data["num_players"]["min"]],
+                                  "num_players_max": [new_obs_data["num_players"]["max"]],
+                                  "play_time_min": [new_obs_data["play_time"]["min"]],
+                                  "play_time_max": [new_obs_data["play_time"]["max"]]})
+    
+    recommend_items = recommendation.rec_model.recommend_from_interactions(new_obs_data_new, k=50)
+    
+    # Select 50 recommended games' info.
+    df_rec_game_info = recommendation.df_items.loc[recommendation.df_items['game_id'].isin(recommend_items['game_id'])]
+    
+    # Filter out game based on user answers.
+    df_items_filter = df_rec_game_info[(df_rec_game_info['age_min']  > filter_condt['age_min'])       &
+                                       (df_rec_game_info['avg_time'] > filter_condt['play_time_min']) & 
+                                       (df_rec_game_info['avg_time'] < filter_condt['play_time_max'])]
+
+    # Convert recommend_items to a dataframe.
+    df_recommend_items=recommend_items.to_dataframe()
+
+    # Output the top 5 games.
+    output      = df_recommend_items.loc[df_recommend_items['game_id'].isin(df_items_filter.game_id)].sort_values('score', ascending=False).head(5)
+    json_output = json.dumps({"game_id": list(output["game_id"]), "score": list(output["score"])})
+    return json_output
+
+APP_FOLDER = os.path.dirname(os.path.realpath(__file__))
